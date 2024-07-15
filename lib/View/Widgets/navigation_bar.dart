c196cc2e4215b40ea9ad8cb8ea6706a3312e3cdb
aliberk <berkyesilduman@gmail.com>
2024-07-15 16:21:44 +0300

    navigasyon düzenlendi. profil sayfası oluşturulacak

--- lib/View/Widgets/navigation_bar.dart
+++ lib/View/Widgets/navigation_bar.dart
@@ -1,35 +1,36 @@
 import 'package:akilli_borsa/View/portf%C3%B6y/portfoy_page.dart';
+import 'package:akilli_borsa/View/profile/profile_page.dart';
 import 'package:akilli_borsa/View/stock%20markets/Markets.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+final RxInt selectedIndex = 1.obs;
 
 class BottomNavigationBarWidget extends StatelessWidget {
-  final RxInt selectedIndex = 1.obs;
 
-   BottomNavigationBarWidget({super.key});
+  BottomNavigationBarWidget({super.key});
+
+  void onItemTapped(int index) {
+    if (selectedIndex.value != index) {
+      selectedIndex.value = index;
+      switch (index) {
+        case 0:
+          Get.offAll(() => const PortfoyPage());
+          break;
+        case 1:
+          Get.offAll(() => Markets());
+          break;
+        case 2:
+          Get.offAll(() => const ProfilePage());
+          break;
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Obx(() => BottomNavigationBar(
       currentIndex: selectedIndex.value,
-      onTap: (index) {
-        selectedIndex.value = index;
-
-        switch (index) {
-          case 0:
-            Get.to(const PortfoyPage());
-            break;
-          case 1:
-            if (selectedIndex.value != 1) {
-                  Get.to(
-                       Markets());
-                }
-                break;
-          case 2:
-            Get.toNamed('/profile');
-            break;
-        }
-      },
+      onTap: onItemTapped,
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.account_balance_wallet_outlined),
---
+++ lib/View/profile/profile_page.dart
@@ -0,0 +1,22 @@
+import 'package:akilli_borsa/View/Widgets/navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profil', style: TextStyle(color: Colors.white)),
+      ),
+      bottomNavigationBar: BottomNavigationBarWidget(),
+    );
+  }
+}
